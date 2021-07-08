555bbf7f4dd0f16668765d2141d2392d0cfbb618
sovlookup <gonorth@qq.com>
2021-07-08 16:39:23 +0800

    chore: clean dependencies and size window

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,12 +4,6 @@
 )]
 
 fn main() {
-  // todo: starry exit with starries
-  tauri::api::process::Command::new_sidecar("starry")
-    .unwrap()
-    .spawn()
-    .expect("Failed to start starry server");
-
   tauri::Builder::default()
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
